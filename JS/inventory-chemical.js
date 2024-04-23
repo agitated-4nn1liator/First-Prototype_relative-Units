a1301edabb7054ed03d82d2cd860693c5d9bd9c4
Calcifer077 <maheshnashier14@gmail.com>
2024-04-23 20:18:40 +0530

    Added even listener for inventory

--- JS/inventory-chemical.js
+++ JS/inventory-chemical.js
@@ -1,14 +1,203 @@
-const add = document.querySelector('.add-cart-butt');
-const inputChemical1TOtal = document.querySelector('.input-chemical-1-total');
-const inputChemical1 = document.querySelector('.input-chemical-1');
+// Chemical 1
+const addBtn1 = document.querySelector(".input-chemical-1-button");
+const inputChemical1Total = document.querySelector(".input-chemical-1-total");
+const inputChemical1 = document.querySelector(".input-chemical-1-value");
 
-add.addEventListener('click', function(e){
-    e.preventDefault();
-    const totalQuantity = inputChemical1TOtal.textContent;
-    console.log(totalQuantity);
-    // let quantityToBeSubtracted = inputChemical1.value;
-    // let totalQuantity =  Number(inputChemical1TOtal.textContent);
-    // totalQuantity = Number(totalQuantity) -  Number(quantityToBeSubtracted);
-    // inputChemical1TOtal.textContent = Number(totalQuantity);
-}
-)
+addBtn1.addEventListener("click", function (e) {
+  e.preventDefault();
+  const totalQuantity = inputChemical1Total.textContent;
+  //   console.log(totalQuantity);
+
+  let quantityToBeSubtracted = inputChemical1.value;
+  //   console.log(quantityToBeSubtracted);
+
+  inputChemical1Total.textContent = totalQuantity - quantityToBeSubtracted;
+  inputChemical1.value = "";
+});
+
+// Chemical 2
+const addBtn2 = document.querySelector(".input-chemical-2-button");
+const inputChemical2Total = document.querySelector(".input-chemical-2-total");
+const inputChemical2 = document.querySelector(".input-chemical-2-value");
+
+addBtn2.addEventListener("click", function (e) {
+  e.preventDefault();
+  const totalQuantity = inputChemical2Total.textContent;
+  //   console.log(totalQuantity);
+
+  let quantityToBeSubtracted = inputChemical2.value;
+  //   console.log(quantityToBeSubtracted);
+
+  inputChemical2Total.textContent = totalQuantity - quantityToBeSubtracted;
+  inputChemical2.value = "";
+});
+
+// Chemical 3
+const addBtn3 = document.querySelector(".input-chemical-3-button");
+const inputChemical3Total = document.querySelector(".input-chemical-3-total");
+const inputChemical3 = document.querySelector(".input-chemical-3-value");
+
+addBtn3.addEventListener("click", function (e) {
+  e.preventDefault();
+  const totalQuantity = inputChemical3Total.textContent;
+  //   console.log(totalQuantity);
+
+  let quantityToBeSubtracted = inputChemical3.value;
+  //   console.log(quantityToBeSubtracted);
+
+  inputChemical3Total.textContent = totalQuantity - quantityToBeSubtracted;
+  inputChemical3.value = "";
+});
+
+// Chemical 4
+const addBtn4 = document.querySelector(".input-chemical-4-button");
+const inputChemical4Total = document.querySelector(".input-chemical-4-total");
+const inputChemical4 = document.querySelector(".input-chemical-4-value");
+
+addBtn4.addEventListener("click", function (e) {
+  e.preventDefault();
+  const totalQuantity = inputChemical4Total.textContent;
+  //   console.log(totalQuantity);
+
+  let quantityToBeSubtracted = inputChemical4.value;
+  //   console.log(quantityToBeSubtracted);
+
+  inputChemical4Total.textContent = totalQuantity - quantityToBeSubtracted;
+  inputChemical4.value = "";
+});
+
+// Chemical 5
+const addBtn5 = document.querySelector(".input-chemical-5-button");
+const inputChemical5Total = document.querySelector(".input-chemical-5-total");
+const inputChemical5 = document.querySelector(".input-chemical-5-value");
+
+addBtn5.addEventListener("click", function (e) {
+  e.preventDefault();
+  const totalQuantity = inputChemical5Total.textContent;
+  //   console.log(totalQuantity);
+
+  let quantityToBeSubtracted = inputChemical5.value;
+  //   console.log(quantityToBeSubtracted);
+
+  inputChemical5Total.textContent = totalQuantity - quantityToBeSubtracted;
+  inputChemical5.value = "";
+});
+
+// Chemical 6
+const addBtn6 = document.querySelector(".input-chemical-6-button");
+const inputChemical6Total = document.querySelector(".input-chemical-6-total");
+const inputChemical6 = document.querySelector(".input-chemical-6-value");
+
+addBtn6.addEventListener("click", function (e) {
+  e.preventDefault();
+  const totalQuantity = inputChemical6Total.textContent;
+  //   console.log(totalQuantity);
+
+  let quantityToBeSubtracted = inputChemical6.value;
+  //   console.log(quantityToBeSubtracted);
+
+  inputChemical6Total.textContent = totalQuantity - quantityToBeSubtracted;
+  inputChemical6.value = "";
+});
+
+// Chemical 7
+const addBtn7 = document.querySelector(".input-chemical-7-button");
+const inputChemical7Total = document.querySelector(".input-chemical-7-total");
+const inputChemical7 = document.querySelector(".input-chemical-7-value");
+
+addBtn7.addEventListener("click", function (e) {
+  e.preventDefault();
+  const totalQuantity = inputChemical7Total.textContent;
+  //   console.log(totalQuantity);
+
+  let quantityToBeSubtracted = inputChemical7.value;
+  //   console.log(quantityToBeSubtracted);
+
+  inputChemical7Total.textContent = totalQuantity - quantityToBeSubtracted;
+  inputChemical7.value = "";
+});
+
+// Chemical 8
+const addBtn8 = document.querySelector(".input-chemical-8-button");
+const inputChemical8Total = document.querySelector(".input-chemical-8-total");
+const inputChemical8 = document.querySelector(".input-chemical-8-value");
+
+addBtn8.addEventListener("click", function (e) {
+  e.preventDefault();
+  const totalQuantity = inputChemical8Total.textContent;
+  //   console.log(totalQuantity);
+
+  let quantityToBeSubtracted = inputChemical8.value;
+  //   console.log(quantityToBeSubtracted);
+
+  inputChemical8Total.textContent = totalQuantity - quantityToBeSubtracted;
+  inputChemical8.value = "";
+});
+
+// Chemical 9
+const addBtn9 = document.querySelector(".input-chemical-9-button");
+const inputChemical9Total = document.querySelector(".input-chemical-9-total");
+const inputChemical9 = document.querySelector(".input-chemical-9-value");
+
+addBtn9.addEventListener("click", function (e) {
+  e.preventDefault();
+  const totalQuantity = inputChemical9Total.textContent;
+  //   console.log(totalQuantity);
+
+  let quantityToBeSubtracted = inputChemical9.value;
+  //   console.log(quantityToBeSubtracted);
+
+  inputChemical9Total.textContent = totalQuantity - quantityToBeSubtracted;
+  inputChemical9.value = "";
+});
+
+// Chemical 10
+const addBtn10 = document.querySelector(".input-chemical-10-button");
+const inputChemical10Total = document.querySelector(".input-chemical-10-total");
+const inputChemical10 = document.querySelector(".input-chemical-10-value");
+
+addBtn10.addEventListener("click", function (e) {
+  e.preventDefault();
+  const totalQuantity = inputChemical10Total.textContent;
+  //   console.log(totalQuantity);
+
+  let quantityToBeSubtracted = inputChemical10.value;
+  //   console.log(quantityToBeSubtracted);
+
+  inputChemical10Total.textContent = totalQuantity - quantityToBeSubtracted;
+  inputChemical10.value = "";
+});
+
+// Chemical 11
+const addBtn11 = document.querySelector(".input-chemical-11-button");
+const inputChemical11Total = document.querySelector(".input-chemical-11-total");
+const inputChemical11 = document.querySelector(".input-chemical-11-value");
+
+addBtn11.addEventListener("click", function (e) {
+  e.preventDefault();
+  const totalQuantity = inputChemical11Total.textContent;
+  //   console.log(totalQuantity);
+
+  let quantityToBeSubtracted = inputChemical11.value;
+  //   console.log(quantityToBeSubtracted);
+
+  inputChemical11Total.textContent = totalQuantity - quantityToBeSubtracted;
+  inputChemical11.value = "";
+});
+
+// Chemical 12
+const addBtn12 = document.querySelector(".input-chemical-12-button");
+const inputChemical12Total = document.querySelector(".input-chemical-12-total");
+const inputChemical12 = document.querySelector(".input-chemical-12-value");
+
+addBtn12.addEventListener("click", function (e) {
+  e.preventDefault();
+  const totalQuantity = inputChemical12Total.textContent;
+  //   console.log(totalQuantity);
+
+  let quantityToBeSubtracted = inputChemical12.value;
+  //   console.log(quantityToBeSubtracted);
+
+  inputChemical12Total.textContent = totalQuantity - quantityToBeSubtracted;
+  inputChemical12.value = "";
+});
